c43e0b30f28b9dc792eb3a1ade6f66dbf8705dd8
Anderson Ribeiro Lopes <zeander21@outlook.com.br>
2022-12-06 12:06:14 -0300

    Initial tailwind setup

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,38 @@
 <template>
   <div class="container">
     <img src="/MINASEDUCALOGO.png" alt="" />
-  <h1>Aulas</h1>
-  <ul class="wrapper">
-    <a href="./games/joguinho1/game.html">
-      <li class="icon facebook">
-        <span class="tooltip">Jogar!</span>
-        <i class="fab fa-facebook-f">1</i>
-      </li>
-    </a>
-    <a href="./games/joguinho1/game.html">
-      <li class="icon facebook">
-        <span class="tooltip">Jogar!</span>
-        <i class="fab fa-facebook-f">2</i>
-      </li>
-    </a>
-    <a href="./games/joguinho1/game.html">
-      <li class="icon facebook">
-        <span class="tooltip">Jogar!</span>
-        <i class="fab fa-facebook-f">3</i>
-      </li>
-    </a>
-    <a href="./games/joguinho1/game.html">
-      <li class="icon facebook">
-        <span class="tooltip">Jogar!</span>
-        <i class="fab fa-facebook-f">4</i>
-      </li>
-    </a>
-    <a href="./games/joguinho1/game.html">
-      <li class="icon facebook">
-        <span class="tooltip">Jogar!</span>
-        <i class="fab fa-facebook-f">5</i>
-      </li>
-    </a>
-  </ul>
+    <h1>Aulas</h1>
+    <ul class="wrapper">
+      <a href="./games/joguinho1/game.html">
+        <li class="icon facebook">
+          <span class="tooltip">Jogar!</span>
+          <i class="fab fa-facebook-f">1</i>
+        </li>
+      </a>
+      <a href="./games/joguinho1/game.html">
+        <li class="icon facebook">
+          <span class="tooltip">Jogar!</span>
+          <i class="fab fa-facebook-f">2</i>
+        </li>
+      </a>
+      <a href="./games/joguinho1/game.html">
+        <li class="icon facebook">
+          <span class="tooltip">Jogar!</span>
+          <i class="fab fa-facebook-f">3</i>
+        </li>
+      </a>
+      <a href="./games/joguinho1/game.html">
+        <li class="icon facebook">
+          <span class="tooltip">Jogar!</span>
+          <i class="fab fa-facebook-f">4</i>
+        </li>
+      </a>
+      <a href="./games/joguinho1/game.html">
+        <li class="icon facebook">
+          <span class="tooltip">Jogar!</span>
+          <i class="fab fa-facebook-f">5</i>
+        </li>
+      </a>
+    </ul>
   </div>
 </template>
