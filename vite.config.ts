f5ec022183130cb99aab62212174d15c1a8e6d68
Anderson Ribeiro Lopes <zeander21@outlook.com.br>
2022-12-12 10:32:27 -0300

    Add path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -23,5 +24,8 @@ export default defineConfig({
     minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
     // produce sourcemaps for debug builds
     sourcemap: !!process.env.TAURI_DEBUG,
+  },
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
 });
