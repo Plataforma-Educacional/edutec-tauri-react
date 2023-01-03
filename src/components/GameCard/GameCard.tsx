d7da90672e24bc8e626a2ac021e76929711d24a2
Anderson Ribeiro Lopes <zeander21@outlook.com.br>
2023-01-03 17:21:56 -0300

    Change modal to sidepanel

--- src/components/GameCard/GameCard.tsx
+++ src/components/GameCard/GameCard.tsx
@@ -1,12 +1,13 @@
 import React, { useState, Fragment } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import { WebviewWindow } from '@tauri-apps/api/window'
+import XMarkIcon from '@heroicons/react/24/outline/XMarkIcon';
 
 export interface Game {
-    id: number;
-    name: string;
-    href: string;
-    description: string;
+  id: number;
+  name: string;
+  href: string;
+  description: string;
 }
 
 type Props = {
@@ -55,62 +56,89 @@ const GameCard: React.FC<Props> = ({ game }) => {
         <Dialog as="div" className="relative z-10" onClose={setOpen}>
           <Transition.Child
             as={Fragment}
-            enter="ease-out duration-300"
+            enter="ease-in-out duration-500"
             enterFrom="opacity-0"
             enterTo="opacity-100"
-            leave="ease-in duration-200"
+            leave="ease-in-out duration-500"
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <div className="fixed inset-0 bg-ctp-base bg-opacity-75 transition-opacity" />
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
           </Transition.Child>
 
-          <div className="fixed inset-0 z-10 overflow-y-auto">
-            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-out duration-300"
-                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                enterTo="opacity-100 translate-y-0 sm:scale-100"
-                leave="ease-in duration-200"
-                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              >
-                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-ctp-base text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                  <figure className="md:flex rounded-xl p-8 md:p-0">
-                    <img
-                      className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto"
-                      src={game.href + 'thumb.jpg'}
-                      alt={game.name}
-                      width="384"
-                      height="512"
-                    />
-                    <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
-                      <blockquote>
-                        <p className="text-lg font-medium text-ctp-text">
-                          {game.description}
-                        </p>
-                      </blockquote>
-                      <div className="px-4 p-y-10 sm:flex sm:flex-row-reverse sm:px-6">
+          <div className="fixed inset-0 overflow-hidden">
+            <div className="absolute inset-0 overflow-hidden">
+              <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+                <Transition.Child
+                  as={Fragment}
+                  enter="transform transition ease-in-out duration-500 sm:duration-700"
+                  enterFrom="translate-x-full"
+                  enterTo="translate-x-0"
+                  leave="transform transition ease-in-out duration-500 sm:duration-700"
+                  leaveFrom="translate-x-0"
+                  leaveTo="translate-x-full"
+                >
+                  <Dialog.Panel className="pointer-events-auto relative w-screen max-w-2xl">
+                    <Transition.Child
+                      as={Fragment}
+                      enter="ease-in-out duration-500"
+                      enterFrom="opacity-0"
+                      enterTo="opacity-100"
+                      leave="ease-in-out duration-500"
+                      leaveFrom="opacity-100"
+                      leaveTo="opacity-0"
+                    >
+                      <div className="absolute top-0 left-0 -ml-8 flex pt-4 pr-2 sm:-ml-10 sm:pr-4">
                         <button
                           type="button"
-                          className="rounded-md px-4 py-2 text-ctp-base font-medium shadow-sm bg-ctp-green focus:outline-none focus:ring-2 focus:ring-green sm:ml-3 sm:w-auto sm:text-sm"
-                          onClick={() => openGame(game.href + 'index.html')}
-                        >
-                          Jogar
-                        </button>
-                        <button
-                          type="button"
-                          className="rounded-md bg-ctp-overlay0 px-4 py-2 font-medium text-ctp-base sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                          className="rounded-md text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-white"
                           onClick={() => setOpen(false)}
                         >
-                          Fechar
+                          <span className="sr-only">Close panel</span>
+                          <XMarkIcon className="h-6 w-6" aria-hidden="true" />
                         </button>
                       </div>
+                    </Transition.Child>
+                    <div className="flex h-full flex-col overflow-y-scroll bg-ctp-base shadow-xl">
+                      <div className="px-4 sm:px-6 mb-[-106px] z-10 bg-gradient-to-b from-ctp-crust via-ctp-mantle pt-2 pb-12 opacity-90">
+                        <Dialog.Title className="text-lg font-medium text-ctp-text">{game.name}</Dialog.Title>
+                      </div>
+                      <div className="relative mt-6 flex-1">
+                        <div>
+                          <img
+                            className="w-full max-h-96 object-cover object-center md:rounded-none rounded-full mx-auto"
+                            src={game.href + 'thumb.jpg'}
+                            alt={game.name}
+                          />
+                        </div>
+                        <div className="px-6 py-4 text-left space-y-4">
+                          <blockquote>
+                            <p className="text-lg font-medium text-ctp-text">
+                              {game.description}
+                            </p>
+                          </blockquote>
+                          <div className="px-4 p-y-10 sm:flex sm:flex-row-reverse sm:px-6">
+                            <button
+                              type="button"
+                              className="rounded-md px-4 py-2 text-ctp-base font-medium shadow-sm bg-ctp-green focus:outline-none focus:ring-2 focus:ring-green sm:ml-3 sm:w-auto sm:text-sm"
+                              onClick={() => openGame(game.href + 'index.html')}
+                            >
+                              Jogar
+                            </button>
+                            <button
+                              type="button"
+                              className="rounded-md bg-ctp-overlay0 px-4 py-2 font-medium text-ctp-base sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                              onClick={() => setOpen(false)}
+                            >
+                              Fechar
+                            </button>
+                          </div>
+                        </div>
+                      </div>
                     </div>
-                  </figure>
-                </Dialog.Panel>
-              </Transition.Child>
+                  </Dialog.Panel>
+                </Transition.Child>
+              </div>
             </div>
           </div>
         </Dialog>
